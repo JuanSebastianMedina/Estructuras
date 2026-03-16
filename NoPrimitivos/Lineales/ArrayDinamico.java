04b1dd8e95b4e8a9c1bc36a21f2eca42b4408ca8
JuanSebastianMedina <jmedinapineros@gmail.com>
2026-03-16 12:23:21 -0500

    incorpora documentacion Array Dinamica y Array Dinamica Java

---
+++ NoPrimitivos/Lineales/ArrayDinamico.java
@@ -0,0 +1,14 @@
+// importa la clase
+import java.util.ArrayList;
+public class ArrayDinamico {
+    public static void main(String[] args) {
+        // crea el objeto sin dimension clara
+        ArrayList<Integer> lista = new ArrayList<>();
+        // añade elementos
+        lista.add(10);
+        lista.add(20);
+        lista.add(30);
+        // implementa lista.toString() para imprimir
+        System.out.println(lista);
+    }
+}
