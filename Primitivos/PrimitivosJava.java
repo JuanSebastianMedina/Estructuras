117cc7f84871ded625de7ced08de6d2946242f5b
JuanSebastianMedina <jmedinapineros@gmail.com>
2026-03-15 19:26:48 -0500

    incorpora documentacion primitivos y primituvos Java

---
+++ Primitivos/PrimitivosJava.java
@@ -0,0 +1,27 @@
+
+public class PrimitivosJava {
+    public static void main(String[] args) {
+
+        byte edad = 25;
+        short anio = 2025;
+        int poblacion = 8000000;
+        long distanciaEstrellas = 922337203685477580L;
+
+        float temperatura = 36.6f;
+        double pi = 3.141592653589793;
+
+        char letra = 'A';
+        boolean esProgramador = true;
+
+        System.out.println("Edad: " + edad);
+        System.out.println("Año: " + anio);
+        System.out.println("Población: " + poblacion);
+        System.out.println("Distancia estrellas: " + distanciaEstrellas);
+
+        System.out.println("Temperatura: " + temperatura);
+        System.out.println("Pi: " + pi);
+
+        System.out.println("Letra: " + letra);
+        System.out.println("Es programador: " + esProgramador);
+    }
+}
